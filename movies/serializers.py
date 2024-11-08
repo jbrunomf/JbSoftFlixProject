20e1413d024010026d8407fcbb5f30a512277681
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-11-07 21:28:16 -0400

    obtendo rating do filme a partir do Avg

--- movies/serializers.py
+++ movies/serializers.py
@@ -13,14 +13,16 @@ class MovieSerializer(serializers.ModelSerializer):
         model = Movie
         fields = '__all__'
 
-    def get_rate(self, obj):
-        return round(obj.reviews.aggregate(average=Avg('rating'))['average'], 1) if obj.reviews.exists() else None
-        
 
-    def validate_title(self, value: str):
-        if not value.lower().startswith('a'):
-            raise serializers.ValidationError('O título deve iniciar com a letra A')
+def get_rate(self, obj):
+    return round(obj.reviews.aggregate(average=Avg('rating'))['average'], 1) if obj.reviews.exists() else None
 
-    def validate_release_date(self, value: datetime):
-        if not value.year >= 1990:
-            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
+
+def validate_title(self, value: str):
+    if not value.lower().startswith('a'):
+        raise serializers.ValidationError('O título deve iniciar com a letra A')
+
+
+def validate_release_date(self, value: datetime):
+    if not value.year >= 1990:
+        raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
