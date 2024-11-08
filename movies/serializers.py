b32331a30e3519de01acc5f739b431c13cd321f3
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-11-07 21:29:56 -0400

    Disable title validation on movie serializer
    Commented out the title validation method that enforced titles to start with the letter 'A'. This change is aimed at allowing more flexibility in movie titles during serialization.

--- movies/serializers.py
+++ movies/serializers.py
@@ -18,9 +18,9 @@ def get_rate(self, obj):
     return round(obj.reviews.aggregate(average=Avg('rating'))['average'], 1) if obj.reviews.exists() else None
 
 
-def validate_title(self, value: str):
-    if not value.lower().startswith('a'):
-        raise serializers.ValidationError('O título deve iniciar com a letra A')
+# def validate_title(self, value: str):
+#     if not value.lower().startswith('a'):
+#         raise serializers.ValidationError('O título deve iniciar com a letra A')
 
 
 def validate_release_date(self, value: datetime):
