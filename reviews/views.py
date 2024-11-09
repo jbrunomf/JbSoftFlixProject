97afa906659aa3aa19fa6396c430e0881bdf53d9
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-11-08 21:58:56 -0400

    Add authentication to views and token verification endpoint
    Enforce authentication for Actor, Review, Genre, and Movie views to enhance security. Added a new endpoint for verifying JWT tokens to the authentication URLs for improved token management.

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from reviews.models import Review
 from reviews.serializers import ReviewModelSerializer
+from rest_framework import permissions
 
 
 class ReviewListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
