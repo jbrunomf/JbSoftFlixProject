b16f213fbd42ef9d5e86d72be50a8a68dfd0ee5b
João Bruno M. Fernandes <jbrunomf@outlook.com>
2024-11-20 19:50:09 -0400

    Add MovieStatisticsView for movie and review statistics
    This commit introduces a new endpoint, `MovieStatisticsView`, to provide statistics on movies and reviews. The view calculates total movies, movies by genre, and average review ratings, improving the analytics capabilities of the application. Updates are also made to the URL configuration to include this new statistics endpoint.

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
+from rest_framework.permissions import IsAuthenticated
 
 from movies.models import Movie
 
@@ -7,6 +9,9 @@ from movies.serializers import MovieSerializer
 from rest_framework import permissions
 
 from djangoDrfLearningProject.defaultPermission import DefaultPermission
+
+from reviews.models import Review
+
 
 class MovieListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated, DefaultPermission]
@@ -20,3 +25,19 @@ class MovieRetrieveUpdateDestroyApiView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MovieSerializer
 
 
+class MovieStatisticsView(views.APIView):
+    permission_classes = (IsAuthenticated, DefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values("genre__name").annotate(count=Count("id"))
+        total_reviews = Review.objects.count()
+        average_reviews = Review.objects.aggregate(avg_reviews=Avg("rating"))['avg_reviews']
+
+        return response.Response(data={
+            "total_movies": total_movies,
+            "total_movies by genre": movies_by_genre,
+            "total reviews": total_reviews,
+            "average reviews": round(average_reviews, 1) if average_reviews else 0,
+        }, status=status.HTTP_200_OK)
